e574cbdffbb84fa5c3456569bbb4d6e04c660d72
Muhammad Ziyad Khan <ziyadkhan2333@gmail.com>
2023-08-21 14:43:59 +0500

    basic store api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Users;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = Users::all();
+        return response()->json([
+            'users' => $user,
+            'message' => 'hello'
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $user_name = $request->name;
+        User::create(['name' => $user_name]);
+        return response()->json([
+            'message' => 'Data added Successfully'
+        ]);
+    }
 }
